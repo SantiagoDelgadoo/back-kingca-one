e0c5a4294b568bf29ad06b4dc9697fe56abfede1
SantiagoDelgadoo <santiagodelgado.2003@gmail.com>
2022-11-10 18:05:58 -0300

    cree modelo activities

---
+++ models/Activity.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose') //requiero el paquete que estamos usando (mongoose)
+const schema = new mongoose.Schema({ //creo el esquema que necesita el modelo
+    //propiedades de mi estructura de datos, a cada una le indico el tipo de dato que va a ser y especifico si es requerido o no es requerido
+    cityid: {type: mongoose.Types.ObjectId,ref:'cities', required: true}, //le paso id de la base de datos de users
+    name: {type: String, required: true},
+    photo: {type: String, required: true},
+    description: {type: String, required: true},
+    price:{type: Number, required: true},
+    duration: {type: Number, required: true},
+    userId: {type: mongoose.Types.ObjectId,ref:'users', required: true}, //le paso id de la base de datos de users
+}) 
+
+const Activity = mongoose.model('activities',schema)
+module.exports= Activity
