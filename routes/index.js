eabb42fb0b1c9a7398d767d23b846c288600a263
SantiagoDelgadoo <santiagodelgado.2003@gmail.com>
2022-11-11 14:05:43 -0300

    mt-01-18

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 let router = require ('express').Router () //requiero el metodo de enrrutamiento de express
 
 let user = require ('./user') //ubicacion de las rutas de user
+let city = require ('./city') //ubicacion de las rutas de user
 
 router.use('/user',user) //le obligo el enrrutador que use la palabra ./user para controlar las rutas de user 
 
+router.use('/city',city) //le obligo el enrrutador que use la palabra ./user para controlar las rutas de user 
 
 
 
