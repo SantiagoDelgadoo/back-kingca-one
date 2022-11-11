0698e18fd4c2ccc2d9718d91dd767b4a68f3b3ef
HernanC95 <cavalieri.h95@gmail.com>
2022-11-11 13:42:08 -0300

    update Create Events

--- models/data/events.js
+++ models/data/events.js
@@ -6,7 +6,7 @@ let events = [
         photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F7%2F7c%2FMiranda%2521_en_el_2021.jpg&imgrefurl=https%3A%2F%2Fes.wikipedia.org%2Fwiki%2FMiranda!&tbnid=LF2lWh5a_iZoeM&vet=12ahUKEwiJ4eqEvY77AhXGMbkGHdB4ATEQMygAegUIARDlAQ..i&docid=vmNq6kTeUXPAJM&w=580&h=403&q=foto%20de%20miranda&ved=2ahUKEwiJ4eqEvY77AhXGMbkGHdB4ATEQMygAegUIARDlAQ",
         price: 2000,
         date: "2023-10-04",
-        userId: "user1",
+        userId: "636d8957667c3ec43577d469",
     },
     {
         hotelId: "636d5ff16abdddfa7405ab43",
@@ -15,7 +15,7 @@ let events = [
         photo: "https://imagenes.elpais.com/resizer/q00YUVJGRnnxbXnJWbXrHrI1Lq8=/1200x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/DSO7P6I6KJE7ZCHQYATNYNFLFU.jpg",
         price: 5000,
         date: "2023-8-04",
-        userId: "user2",
+        userId: "636e644400607058e7cf0ba8",
     },
     {
         hotelId: "636d5ff16abdddfa7405ab44",
@@ -24,7 +24,7 @@ let events = [
         photo: "https://nexodiario.com/wp-content/uploads/2018/08/midachi-2.jpg",
         price: 2500,
         date: "2023-06-04",
-        userId: "user3",
+        userId: "636e662900607058e7cf0baa",
     },
     {
         hotelId: "636d5ff16abdddfa7405ab44",
@@ -32,7 +32,7 @@ let events = [
         photo: "https://i1.sndcdn.com/artworks-h4r5dxHU1pSk7U8F-cKoYuQ-t500x500.jpg",
         price: 1500,
         date: "2023-11-14",
-        userId: "user4",
+        userId: "636e68db00607058e7cf0bac",
     },
     {
         hotelId: "636d5ff16abdddfa7405ab45",
@@ -40,7 +40,7 @@ let events = [
         photo: "https://www.sala-apolo.com/uploads/media/default/0001/03/thumb_2993_default_wide.jpeg",
         price: 4000,
         date: "2023-5-20",
-        userId: "user5",
+        userId: "636d8957667c3ec43577d469",
     },
     {
         hotelId: "636d5ff16abdddfa7405ab45",
@@ -48,7 +48,7 @@ let events = [
         photo: "https://www.larepublica.ec/wp-content/uploads/2014/10/guetta2.png",
         price: 6500,
         date: "2023-03-28",
-        userId: "user6",
+        userId: "636e644400607058e7cf0ba8",
     },
     {
         hotelId: "636d5ff16abdddfa7405ab48",
@@ -56,7 +56,7 @@ let events = [
         photo: "https://static.wikia.nocookie.net/lossimpson/images/0/02/U26.jpg/revision/latest?cb=20111220210807&path-prefix=es",
         price: 10000,
         date: "2023-05-13",
-        userId: "user7",
+        userId: "636e662900607058e7cf0baa",
     },
     {
         hotelId: "636d5ff16abdddfa7405ab48",
@@ -64,7 +64,7 @@ let events = [
         photo: "https://www.nacion.com/resizer/poPw_EJNVQDwUs2kyXKFF8qRHOE=/1440x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/gruponacion/VK45RJBFHVDJZNF7ND7G4CTHYU.jpeg",
         price: 1350,
         date: "2023-09-06",
-        userId: "user8",
+        userId: "636e68db00607058e7cf0bac",
     },
     {
         hotelId: "636d5ff16abdddfa7405ab46",
@@ -72,7 +72,7 @@ let events = [
         photo: "https://yt3.ggpht.com/ytc/AMLnZu_q_r5IDfw5uZEOrh9ZWZCD-K8Ab_9ZhF-aag9J=s900-c-k-c0x00ffffff-no-rj",
         price: 8000,
         date: "2023-12-04",
-        userId: "user9",
+        userId: "636d8957667c3ec43577d469",
     },
     {
         hotelId: "636d5ff16abdddfa7405ab46",
@@ -80,7 +80,7 @@ let events = [
         photo: "https://i.guim.co.uk/img/media/21de53d9a63650d597c812f2b8e4695a3fc88369/0_82_3129_1877/master/3129.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=4cff2d79ea769f32f59db1bc6063ac8b",
         price: 1000,
         date: "2023-01-21",
-        userId: "user10",
+        userId: "636e644400607058e7cf0ba8",
     },
     {
         hotelId: "636d5ff16abdddfa7405ab42",
@@ -88,7 +88,7 @@ let events = [
         photo: "https://i.guim.co.uk/img/media/d7428a87e15c1a6b0725d17a8b0a7ff5175372ee/1161_971_1824_1094/master/1824.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=43a30872b9c7fd4b198d3f351c45bad3",
         price: 3000,
         date: "2023-06-19",
-        userId: "user11",
+        userId: "636e662900607058e7cf0baa",
     },
     {
         hotelId: "636d5ff16abdddfa7405ab42",
@@ -96,7 +96,7 @@ let events = [
         photo: "https://dynamicmedia.livenationinternational.com/Media/g/p/o/eb285dd6-45d0-4f98-9ab5-aa7cc8075a98.jpg?auto=webp&width=1507.2",
         price: 12000,
         date: "2023-07-01",
-        userId: "user12",
+        userId: "636d8957667c3ec43577d469",
     },
 ]
 
