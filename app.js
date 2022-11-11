d626bcb063566daf89f53e3fb41058c660fc6cbe
SantiagoDelgadoo <santiagodelgado.2003@gmail.com>
2022-11-11 14:57:18 -0300

    agregue cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require ('dotenv').config ()
+const cors = require('cors');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -20,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 app.use('/api', indexRouter);
 
 //borro la confi del enrrutador de usuarios
