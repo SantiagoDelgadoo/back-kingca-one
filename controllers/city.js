30883d1156f17779a71c1e28c89d52c3a721e6ca
SantiagoDelgadoo <santiagodelgado.2003@gmail.com>
2022-11-12 17:19:43 -0300

    mt-b1-29 terminada

--- controllers/city.js
+++ controllers/city.js
@@ -19,6 +19,26 @@ const controller ={
             })
         }
         
-        }
+        },
+
+        deleteCity: async (req,res) =>{ //la funcion async siempre depende de dos parametros, REQ requerimiento de la peticion y RES respuesta de la peticion
+            let {id} = req.params
+            try {
+            let delete_city = await City.findOneAndDelete({_id: id})
+            res.status(201).json({//el status de exito de creacion es 201
+                id: delete_city._id,
+                success: true,
+                messagge: 'cityDelete'
+            }) 
+            
+            } catch (error) {
+                res.status(400).json({
+                    success: false,
+                    messagge: 'cityNoDelete',
+                    error: error.status
+                })
+            }
+            
+            },
 }
 module.exports = controller //exporto el controlador
