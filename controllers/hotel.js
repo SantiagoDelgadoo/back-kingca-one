c8850bdb729d6a57e77556d572f576cfc32da37a
HernanC95 <cavalieri.h95@gmail.com>
2022-11-14 09:50:30 -0300

    create destroy controller

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -1,30 +1,51 @@
 //primero requiero el modelo que necesito para controlar
-const Hotel = require('../models/Hotel')
+const Hotel = require("../models/Hotel");
 // segundo defino el objeto controller (que va a controlar el modelo)
 
-const controller={
-    //cada uno de los metodos son async
-    //metodo para crear un documento de Usuario
-    create: async(req,res)=>{
-        try {
-            let new_hotel= await Hotel.create(req.body)
-            res.status(201).json({
-                id: new_hotel._id,
-                success: true,
-                message:"el usuario se creo satisfactoriamente"
-            })
-
-            } catch (error) {
-            res.status(400).json(
-                {
-                    success: false,
-                    message: error.message
-                }
-            )
-        }
+const controller = {
+  //cada uno de los metodos son async
+  //metodo para crear un documento de Usuario
+  create: async (req, res) => {
+    try {
+      let new_hotel = await Hotel.create(req.body);
+      res.status(201).json({
+        id: new_hotel._id,
+        success: true,
+        message: "el usuario se creo satisfactoriamente",
+      });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
     }
-}
+  },
 
+  destroy: async (req, res) => {
+    let { id } = req.params;
+    try {
+      let hotel = await Hotel.findOneAndDelete({ _id: id });
+      if (hotel) {
+        res.status(201).json({
+          id: hotel._id,
+          success: true,
+          messagge: "Delete Hotel",
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          messagge: "Hotel not found",
+        });
+      }
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        messagge: "No Delete Hotel",
+        error: error.status,
+      });
+    }
+  },
+};
 
 //tercero exporto el controlador
-module.exports = controller
+module.exports = controller;
--- routes/hotel.js
+++ routes/hotel.js
@@ -1,6 +1,7 @@
-let router= require('express').Router()
-let {create} = require('../controllers/hotel')
+let router = require("express").Router();
+let { create, destroy } = require("../controllers/hotel");
 
-router.route('/').post(create)
+router.route("/").post(create);
+router.route("/:id").delete(destroy);
 
 module.exports = router;
