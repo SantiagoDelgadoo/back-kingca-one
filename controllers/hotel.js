5ce751900665550ea34a2d863e3dfa376e012c1a
HernanC95 <cavalieri.h95@gmail.com>
2022-11-14 09:19:54 -0300

    create Method get in Hotel Controller

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -22,6 +22,44 @@ const controller={
                 }
             )
         }
+    },
+
+    read: async (req,res) => {
+        let query={}
+        let order = {}
+        if (req.query.name){
+            query=req.query.name
+        }
+        if (req.query.order){
+            order={name:req.query.order}
+        }
+
+        console.log(req.query);
+            try {
+            let arrayHotels = await Hotel.find(query).sort(order)
+            if (arrayHotels){
+                res.status(200).json({
+                    Response : arrayHotels,
+                    success: true,
+                    message:"el usuario se creo satisfactoriamente"    
+                })
+                }
+                else{
+                    res.status(404).json({
+                        success: false,
+                        message:"no hay usuarios"    
+                    })  
+                }
+           
+            
+        } catch (error) {
+            res.status(400).json(
+                {
+                    success: false,
+                    message: error.message
+                }
+            )
+        }
     }
 }
 
