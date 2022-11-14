40c26c40010c060eef1df0e832f986c861e74e03
SantiagoDelgadoo <santiagodelgado.2003@gmail.com>
2022-11-14 17:42:56 -0300

    termine la parte de back de la task mt-01-19

--- routes/city.js
+++ routes/city.js
@@ -1,10 +1,11 @@
 let router = require ('express').Router () //requiero el metodo de enrrutamiento de express
 const controllers = require ('../controllers/city')
-let { create,deleteCity,EditCity } = require ('../controllers/city') //desestructuro el controlador de user para traerme los metodos que necesito enrrutar, mas adelante si quier agregar alguno los voy a poder agregar dentro de las {} y utilizando una , para separarlos
+let { create,deleteCity,EditCity,SeeCity } = require ('../controllers/city') //desestructuro el controlador de user para traerme los metodos que necesito enrrutar, mas adelante si quier agregar alguno los voy a poder agregar dentro de las {} y utilizando una , para separarlos
 
 router.route('/').post(create) //en esta ruta voy a crear los usuarios y uso post para postear el usuario creado 
 router.route('/:id').delete(deleteCity) //en esta ruta voy a crear los usuarios y uso post para postear el usuario creado 
 router.route('/:id').put(EditCity) //en esta ruta voy a crear los usuarios y uso post para postear el usuario creado 
+router.route('/').get(SeeCity) //en esta ruta voy a crear los usuarios y uso post para postear el usuario creado 
 
 
 
